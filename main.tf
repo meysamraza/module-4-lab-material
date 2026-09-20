5e7cb8f5f09743fbcf342b767817dff69448feda
Meysam <arazameysam@gmail.com>
2026-09-20 22:35:25 +0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_iam_role" "lab2_tf_role" {
+  name = "lab2-tf-drift-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lab2_tf_policy" {
+  name = "lab2-tf-policy"
+  role = aws_iam_role.lab2_tf_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["s3:GetObject", "s3:ListBucket"]
+        Resource = "*"
+      }
+    ]
+  })
+}
